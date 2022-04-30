dad02ecc2eb167b10a2400293e0ae55911556476
MagnetarRocket <53055079+MagnetarRocket@users.noreply.github.com>
2022-04-30 00:23:42 -0500

    Added example app

---
+++ Source/NotSerious.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+//not_serious_"arch" is ment for giving meat to legal arguments about licence, delete if you wish
+
+//this simple app itself is under the MIT
+
+int main(void)
+{
+	printf("\n delete this program if you intend to use this script, this program is here for testing and licensing \n");
+	return 0;
+}
