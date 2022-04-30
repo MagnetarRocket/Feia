2b804d47ff521d5436deecdb83b69d84d6620a49
MagnetarRocket <53055079+MagnetarRocket@users.noreply.github.com>
2022-04-30 00:48:40 -0500

    Added needed spaces

--- feia_run.sh
+++ feia_run.sh
@@ -3,28 +3,28 @@
 # note that IBM unixes show serial numbers only on "-m", this script is also not for them at all. This script is ment for 
 # real-time graphics programs like video-games running on unix.
 
-case "$(uname -s -m)"in 
+case "$(uname -s -m)" in 
 	Darwin arm64)
 		echo"runing for darwin on arm_64"
 		./Architecture/Aarch64/Darwin/not_serious_del
 	;;
   
 	Linux aarch64)
-		echo"runing for linux on arm_64"
+		echo "runing for linux on arm_64"
 		./
 	;;
   
 	Darwin x86_64)
-		echo"runing for darwin on x86_64"
+		echo "runing for darwin on x86_64"
 		./Architecture/Aarch64/Darwin/not_serious_delx8664
 	;;
   
 	Linux x86_64)
-		echo"runing for linux on x86_64"
+		echo "runing for linux on x86_64"
 		./
 	;;
   
 	*)
-		echo"This program is not compatible, please read the manual that came with it or ask online."
+		echo "This program is not compatible, please read the manual that came with it or ask online."
 	;;
 esac
